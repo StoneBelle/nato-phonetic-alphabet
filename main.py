b70f50c4f7da1068ae8a6764b799f843c08a4a75
Stone <65476500+StoneBelle@users.noreply.github.com>
2022-06-01 14:43:18 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # 25 May 2022
+import pandas
 
 student_dict = {
     "student": ["Angela", "James", "Lily"],
@@ -10,12 +11,9 @@ for (key, value) in student_dict.items():
     # Access key and value
     pass
 
-import pandas
-
 student_data_frame = pandas.DataFrame(student_dict)
 
 # Loop through rows of a data frame
-
 for (index, row) in student_data_frame.iterrows():
     # Access index and row
     # Access row.student or row.score
@@ -28,15 +26,22 @@ for (index, row) in student_data_frame.iterrows():
 # {"A": "Alfa", "B": "Bravo"}
 nato_df = pandas.read_csv("nato_phonetic_alphabet.csv")
 for (index, row) in nato_df.iterrows():
-    #print(index)
-    #print(row.letter)
-    nato_alphabet_dict= {row.letter: row.code for (index, row) in nato_df.iterrows()}
+    # print(index)
+    # print(row.letter)
+    nato_alphabet_dict = {row.letter: row.code for (index, row) in nato_df.iterrows()}
     # print(nato_alphabet_dict)
 
-# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-change_this = input("Enter a word to be transcribed into the NATO phonetic alphabet: ").upper()
-print(change_this)
 
-changed_word = [letter for letter in change_this]
-changed_word_NATO = [nato_alphabet_dict[letter] for letter in changed_word]
-print(changed_word_NATO)
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    change_this = input("Enter a word to be transcribed into the NATO phonetic alphabet: ").upper()
+    try:
+        changed_word = [nato_alphabet_dict[letter] for letter in change_this]
+    except KeyError:
+        print("Only input letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(changed_word)
+
+
+generate_phonetic()
